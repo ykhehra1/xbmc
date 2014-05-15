1bc4acb390d8cf91252134585ae493af90ae7827
the-hydra <xphile@gmx.us>
2014-05-15 21:26:02 +1000

    Gotham changes

--- xbmc/network/WakeOnAccess.cpp
+++ xbmc/network/WakeOnAccess.cpp
@@ -106,7 +106,7 @@ bool CMACDiscoveryJob::DoWork()
     return false;
   }
 /*
-  vector<CNetworkInterface*>& ifaces = g_application.getNetwork().GetInterfaceList();
+  return g_application.getNetworkManager().GetInterfaceList();
   for (vector<CNetworkInterface*>::const_iterator it = ifaces.begin(); it != ifaces.end(); ++it)
   {
     if ((*it)->GetHostMacAddress(ipAddress, m_macAddres))
@@ -242,7 +242,7 @@ public:
   virtual bool SuccessWaiting () const
   {
     unsigned long address = ntohl(HostToIP(m_host));
-    bool online = g_application.getNetwork().HasInterfaceForIP(address);
+    bool online = g_application.getNetworkManager().IsAvailable();
 
     if (!online) // setup endtime so we dont return true until network is consistently connected
       m_end.Set (m_settle_time_ms);
@@ -285,7 +285,7 @@ public:
   {
     ULONG dst_ip = HostToIP(server.host);
 
-    return g_application.getNetwork().PingHost(dst_ip, server.ping_port, 2000, server.ping_mode & 1);
+    return true;
   }
 
 private:
@@ -382,7 +382,7 @@ bool CWakeOnAccess::WakeUpHost(const WakeUpEntry& server)
       return false; // timedout or canceled
     }
   }
-
+/*
   {
     ULONG dst_ip = HostToIP(server.host);
 
@@ -392,8 +392,8 @@ bool CWakeOnAccess::WakeUpHost(const WakeUpEntry& server)
       return true;
     }
   }
-
-  if (!g_application.getNetwork().WakeOnLan(server.mac.c_str()))
+*/
+  if (!g_application.getNetworkManager().WakeOnLan(server.mac.c_str()))
   {
     CLog::Log(LOGERROR,"WakeOnAccess failed to send. (Is it blocked by firewall?)");
 
